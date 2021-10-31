03fc296e34ebd2f0a6cb8b49ea7833e7bff0651c
anekemichael <anekemikeobiora2@gmail.com>
2021-10-31 17:27:51 +0100

    commit changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,60 @@
 //Creating my first blockchain using javascript
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
 
-class Block{
-    constructor(timestamp = "", data =[]){
+class Block {
+    constructor(timestamp = Date.now().toString(), data = []) {
         this.timestamp = timestamp;
-        //this.data should contain information like transactions.
         this.data = data;
+        this.prevHash = "";
+        this.hash = this.getHash();
+        this.nonce = 0;
+    }
+
+    getHash() {
+        return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data) + this.nonce);
+    }
+
+    mine(difficulty) {
+        while(!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
+            this.nonce++;
+            this.hash = this.getHash();
+        }
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [new Block()];
+        this.difficulty = 1;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(block) {
+        block.prevHash = this.getLastBlock().hash;
+        block.hash = block.getHash();
+        block.mine(this.difficulty);
+        this.chain.push(block);
+    }
+
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const prevBlock = this.chain[i-1];
+
+            if (currentBlock.hash !== currentBlock.getHash() || prevBlock.hash !== currentBlock.prevHash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+const JeChain = new Blockchain();
+
+module.exports.Block = Block;
+module.exports.Blockchain = Blockchain;
+module.exports.JeChain = JeChain;
